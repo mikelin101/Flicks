f485806f4d0e2b3aef6d13242e4342f60cf721d0
Michael Lin <mili@yahoo-inc.com>
2017-01-25 14:01:58 -0800

    selecting items opens new screen

---
+++ Flicks/MovieDetailsController.h
@@ -0,0 +1,13 @@
+//
+//  MovieDetailsController.h
+//  Flicks
+//
+//  Created by  Michael Lin on 1/25/17.
+//  Copyright © 2017 codepath. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface MovieDetailsController : UIViewController
+
+@end
---
+++ Flicks/MovieDetailsController.m
@@ -0,0 +1,13 @@
+//
+//  MovieDetailsController.m
+//  Flicks
+//
+//  Created by  Michael Lin on 1/25/17.
+//  Copyright © 2017 codepath. All rights reserved.
+//
+
+#import "MovieDetailsController.h"
+
+@implementation MovieDetailsController
+
+@end
